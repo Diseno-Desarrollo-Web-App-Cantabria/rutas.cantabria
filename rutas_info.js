af76158a0784e4d001b7b09b6c75d9fb97c9448d
Diseño Web Cantabria | Desarrollo Web Cantabria <62180604+Diseno-Desarrollo-Web-App-Cantabria@users.noreply.github.com>
2020-05-03 17:17:53 +0200

    Update rutas_info.js

--- rutas_info.js
+++ rutas_info.js
@@ -4,5 +4,9 @@
 
  "dfficulty" : [
         "Media"
-     ]
+     ],
+     
+ "img" : [
+        "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/llanos_lloreda.jpg"
+     ]    
  }                                        
