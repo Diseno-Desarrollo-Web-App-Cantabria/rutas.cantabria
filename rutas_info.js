f0242806b29999196147d76f7581942994702b01
Diseño Web Cantabria | Desarrollo Web Cantabria <62180604+Diseno-Desarrollo-Web-App-Cantabria@users.noreply.github.com>
2020-05-03 18:27:20 +0200

    Update rutas_info.js

--- rutas_info.js
+++ rutas_info.js
@@ -1,7 +1,7 @@
 {
-  "name"      : [ "Lloreda - LLanos" ],
-  "dfficulty" : [ "Dificultad media" ],
+  "name"      : [ "Nombre: Lloreda - LLanos" ],
+  "dfficulty" : [ "Dificultad: media." ],
   "img"       : [ "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/llanos_lloreda.jpg" ],
-  "distance"  : [ "Distancia 17198 metros" ], 
-  "time"      : [ "Tiempo de media 4 horas 30 minutos" ]    
+  "distance"  : [ "Distancia: 17198 metros." ], 
+  "time"      : [ "Tiempo de media: 4 horas 30 minutos." ]    
  }                                        
