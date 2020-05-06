6119bbdea164d2802f5d093f7c5eb443f70865c2
Diseño Web Cantabria | Desarrollo Web Cantabria <62180604+Diseno-Desarrollo-Web-App-Cantabria@users.noreply.github.com>
2020-05-06 19:19:10 +0200

    Update rutas_info.js

--- rutas_info.js
+++ rutas_info.js
@@ -1,6 +1,6 @@
 {
   "name"      : [ "Lloreda - LLanos" , 
-                 "Pozos de Noja -La Estranguada"
+                 "Pozos de Noja - La Estranguada"
                 ],
   "dfficulty" : [ "Dificultad: media.",
                   "Dificultad: fácil."
@@ -9,7 +9,7 @@
                   "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/img/pozo_noja.jpg"
                 ],
   "distance"  : [ "Distancia: 17198 metros.", 
-                  "Distancia:8082 metros"
+                  "Distancia: 8082 metros"
                 ], 
   "time"      : [ "Tiempo de media: 4 horas 30 minutos.", 
                  "Tiempo de media: 2 horas."
