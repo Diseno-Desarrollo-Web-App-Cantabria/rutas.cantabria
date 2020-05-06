6ccb927bf8a01cf54e0afd9b5cc4002a52b48a30
Diseño Web Cantabria | Desarrollo Web Cantabria <62180604+Diseno-Desarrollo-Web-App-Cantabria@users.noreply.github.com>
2020-05-06 16:56:48 +0200

    Update rutas_info.js

--- rutas_info.js
+++ rutas_info.js
@@ -1,12 +1,12 @@
 {
   "name"      : [ "Lloreda - LLanos" , 
-                 "test"
+                 "Pozos de Noja"
                 ],
   "dfficulty" : [ "Dificultad: media.",
-                 "test"
+                  "Dificultad: fácil."
                 ],
   "img"       : [ "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/img/llanos_lloreda.jpg",
-                "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/img/pozo_noja.jpg"
+                  "https://diseno-desarrollo-web-app-cantabria.github.io/rutas.cantabria/img/pozo_noja.jpg"
                 ],
   "distance"  : [ "Distancia: 17198 metros.", 
                  "test"
